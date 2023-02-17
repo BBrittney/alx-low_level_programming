cab9d5efd4bf3a07b32a67681255a82992df2b30
BBrittney <brittneybanks005@gmail.com>
2023-02-16 17:10:06 -0800

    Alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main -To print out the sizes of data typs in C
+ * main - To print out the sizes of data typs in C
  *
- * Return:0
+ * Return: 0
 */
 int main(void)
 {
-        int a;
-	long int b;
+        char a;
+	int b;
+	long int c;
 	long long int d;
-	char e;
-	float c;
+	float f;
 	
-	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
         return (0);
 }
